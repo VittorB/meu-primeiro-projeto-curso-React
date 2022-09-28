a70eb9a6d6689b526e90ee6a15619bcc20fae828
VittorB <vittor.lucahaddadbeck@gmail.com>
2022-09-27 21:00:36 -0300

    Começo do campotexto

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import Banner from './componentes/Banner/Banner';
+import Banner from './componentes/Banner';
+import Campotexto from './componentes/CampoTexto';
 
 function App() {
   return (
     <div className="App">
       <Banner />
-      
+      <Campotexto />
     </div>
   );
 }
---
+++ src/componentes/CampoTexto/index.js
@@ -0,0 +1,10 @@
+const Campotexto = () => {
+    return(
+        <div>
+            <label>Nome</label>
+            <input  />
+        </div>
+    )
+}
+
+export default Campotexto
